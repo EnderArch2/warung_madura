3588d8a22ed191b259791e8bd1c26ce961c163d1
EnderArcherBoy <enderarcherboy@gmail.com>
2025-11-12 14:06:21 +0700

    Belajar Migration dan Model part 5

--- database/migrations/2025_11_12_042901_create_purchases_table.php
+++ database/migrations/2025_11_12_042901_create_purchases_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('purchases', function (Blueprint $table) {
             $table->id();
-            $table->string('note_number', 20)->unique();
+            $table->string('note_number', 15)->unique();
             $table->date('purchase_date');
             $table->foreignId('distributor_id')->constrained('distributors')->onDelete('cascade')->nullAble();
             $table->integer('total_price')->default(0);
